77f4ecc4f79771778279847c3945742de1fe4e0f
FellipeCastro <fellipecastro27d@gmail.com>
2023-12-20 16:12:25 -0300

    Projeto atualizado

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -5,24 +5,26 @@ import './Cart.css'
 export default function Cart({cell, removeToCart, total}) {
     return (
         <div className="cart">
-            {cell.length > 0 ? (
-                cell.map((cell) => {
-                    return (
-                        <div className="item">
-                            <img src={cell.thumbnail} alt={cell.title} />   
-                            <div className="text-container">
-                                <h2>{cell.title}</h2>
-                                <p>R$ {cell.price.toFixed(2)}</p>
+            <div className="items-container">
+                {cell.length > 0 ? (
+                    cell.map((cell) => {
+                        return (
+                            <div className="item">
+                                <img src={cell.thumbnail} alt={cell.title} />
+                                <div className="text-container">
+                                    <h2>{cell.title}</h2>
+                                    <p>R$ {cell.price.toFixed(2)}</p>
+                                </div>
+                                <button className="remove-btn" onClick={() => removeToCart(cell.id)}>
+                                    <BsFillCartDashFill />
+                                </button>
                             </div>
-                            <button className="remove-btn" onClick={() => removeToCart(cell.id)}>
-                                <BsFillCartDashFill />
-                            </button>
-                        </div>
-                    )
-                })
-            ) : (
-                <p className="empty-cart">Seu carrinho esta vazio!</p>
-            )}
+                        )
+                    })
+                ) : (
+                    <p className="empty-cart">Seu carrinho esta vazio!</p>
+                )}
+            </div>
 
             <div className="total-container">
                 <p>Total: R$ {total.toFixed(2)}</p>
